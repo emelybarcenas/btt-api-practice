749a67b25cf73275e85f65a0e31297008a1ef0d8
emelybarcenas <emelyxbarcenas@gmail.com>
2025-03-13 19:23:27 -0400

    updated methods

--- main.py
+++ main.py
@@ -18,14 +18,19 @@ def get_all_tasks():
     return tasks
 
 @app.post("/create-task")
-def create_task():
-    return 
+def create_task(task: Task):
+    tasks.append(task)
+   
 
 @app.put("/update-task")
-def update_task():
-    return 
+def update_task(task_id: int, updated_task: Task):
+    for i, task in enumerate(tasks):
+        if task_id == task_id:
+            tasks[i] = update_task
 
 
 @app.delete("/delete-task")
-def delete_task():
-    return
+def delete_task(task_id: int):
+    for i, task in enumerate(tasks):
+        if task.id == task.id:
+            deleted_task = tasks.pop(i)
